56777a8147be4bc8a2fd950b06f7fd71b13ab159
Akshay E <akshayellath98@gmail.com>
2024-12-06 11:32:11 +0530

    updated end point

--- public/track-event.js
+++ public/track-event.js
@@ -1,5 +1,5 @@
 (function () {
-    const analyticsEndpoint = "https://analytics-iuvzjvfxc-akshayalgoris-projects.vercel.app/api/track";
+    const analyticsEndpoint = "https://analytics-app-sigma.vercel.app/track-event.js/api/track";
 
     function sendEvent(eventName, eventData) {
         fetch(analyticsEndpoint, {
